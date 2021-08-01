a85b9f629436546ce637730d6afe0fc37a2f43ab
ilpech <i.v.pichugin@yandex.ru>
2021-08-01 15:53:34 +0300

    uniprot db connector added

---
+++ tools/uniprot.py
@@ -0,0 +1,26 @@
+import requests, sys
+import json
+
+def getGene(uniprot_id):
+    params = {
+        'accession': uniprot_id,
+        'chromosome': 18
+    }
+    requestURL = "https://www.ebi.ac.uk/proteins/api/coordinates"
+    r = requests.get(
+        requestURL, 
+        headers={"Accept" : "application/json"},
+        params=params
+    )
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
+    responseBody = r.json()
+    out = '../data/api_out/{}.json'.format(uniprot_id)
+    with open(out, 'w') as f:
+        json.dump(responseBody, f)
+    print(responseBody)
+    print('written out', out)
+    
+if __name__ == '__main__':
+    getGene('Q8WU67')
