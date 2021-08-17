737ff365ed0d30418d68ca0b3c78125ef4855e8a
ilpech <i.v.pichugin@yandex.ru>
2021-08-17 21:29:51 +0300

    invalid ctx check fixed

--- tools/dense_train.py
+++ tools/dense_train.py
@@ -15,6 +15,7 @@ class DenseTrainer:
         self.data_loader = DataLoader(config_path)
         try:
             self.ctx = [mx.gpu()]
+            a = mx.nd.array([[0]], ctx=self.ctx[0])
         except:
             self.ctx = [mx.cpu()]
         self.train_settings = self.config['train']
@@ -22,7 +23,7 @@ class DenseTrainer:
         self.batch_settings = self.train_settings['batch_size']
         self.augm_settings = self.train_settings['augm']
         self.with_augm = self.augm_settings['isEnabled']
-        self.epochs_size = self.train_settings['epochs']
+        self.epochs = self.train_settings['epochs']
         self.log_interval = self.train_settings['log_interval']
         self.wd = self.train_settings['wd']
         self.momentum = self.train_settings['momentum']
@@ -30,13 +31,24 @@ class DenseTrainer:
         self.model = DenseProteinPredectionNet(80)
         
     def train_loop(self):
-        optimizer_params = {'wd': self.wd, 'momentum': self.momentum, 'learning_rate': self.lr}
+        lrs = [v for _,v in self.lr_settings.items()]
+        # print(lrs)
+        self.model.collect_params().initialize(mx.init.Xavier(magnitude=2.24), ctx=self.ctx[0])
+        self.model.collect_params().reset_ctx(self.ctx[0])
+        optimizer_params = {
+            'wd': self.wd, 
+            'momentum': self.momentum, 
+            'learning_rate': lrs[0]
+        }
         trainer = mx.gluon.Trainer(
             self.model.collect_params(), 
             self.optimizer, 
             optimizer_params
         )
-        # for i in range(len(epochs)):
+        for i in range(self.epochs):
+            tic = time.time()
+            epoch_time = time.time() - tic
+            print('[Epoch: {}] time: {}'.format(i, epoch_time))
         
         
 if __name__ == '__main__':
@@ -45,6 +57,7 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     trainer = DenseTrainer(opt.config)
     trainer.data_loader.info()
+    trainer.train_loop()
     
     
     
